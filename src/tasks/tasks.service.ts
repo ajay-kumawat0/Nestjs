916373dc1a856272520806846ab5defe34a361ee
Ajay Kumawat <ajay@elemensissoftech.com>
2024-07-22 17:20:18 +0530

    build(service, controller): created addTask and getById task api

---
+++ src/tasks/tasks.service.ts
@@ -0,0 +1,30 @@
+import { Injectable } from '@nestjs/common';
+import { Task, taskStatus } from './task.model';
+import { v4 as uuid } from 'uuid';
+import { createTaskDto } from './DTO/create-task.dto';
+
+@Injectable()
+export class TasksService {
+  private tasks: Task[] = [];
+
+  getAllTasks(): Task[] {
+    return this.tasks;
+  }
+
+  getTaskById(id: string): Task {
+    return this.tasks.find((task) => task.id === id);
+  }
+
+  createTask(createTaskDto: createTaskDto): Task {
+    const { title, description } = createTaskDto;
+    const task: Task = {
+      id: uuid(),
+      title,
+      description,
+      status: taskStatus.DONE,
+    };
+
+    this.tasks.push(task);
+    return task;
+  }
+}
